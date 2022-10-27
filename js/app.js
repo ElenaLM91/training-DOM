08c0ff1b39c30542e440aa25d3f823187c437e8d
Helen <elena-lopezmoreno@hotmail.com>
2022-10-27 13:00:25 +0200

    DOM4

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// document.body.addEventListener("click", ()=>{
+//     alert("This is the body")
+// });
+// document.querySelector("section").addEventListener("click", ()=>{
+//     alert("This is the section")
+// });
+// document.querySelector("div").addEventListener("click", ()=>{
+//     alert("This is the div")
+// });
+document.addEventListener("keydown", (event)=>{
+    event.preventDefault()
+console.log(event.key);
+console.log(event.code);
+if (event.key == "Enter") {
+    alert("Hello World!")
+}
+})
